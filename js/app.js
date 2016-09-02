52fd4af7beb802b73a4eea7e2aa44efe6211962b
Tania A <taniaaallen@gmail.com>
2016-09-02 12:39:37 -0400

    added pseudocode to js file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log("app.js file is connected!");
+//  The Game of Tic Tac Toe
+//  Two players will choose tiles for their letter
+	// playerOne = 'X';
+	// playerTwo = 'O';
+
+// Start Over button should clear the board for players to play the game.
+	// innerText in h3 tag should populate with "It is X's turn"
+
+
+// The first player will always be 'X' when starting the game, playerTwo will always be 'O'
+
+// When the first player clicks a tile:
+	// the innerText of the tile should be updated with the letter of the player ('X')
+	// value should be stored in a variable (document.getElementsByClassName)
+	// data-value of letter should be stored
+	// innerText of the h3 tag should be updated to say 'It is O's turn"
+
+// Next click action should alternate to populate an 'O' in the tile clicked:
+	//Make it so that nothing happens if an already-'clicked' square is clicked
+	//Hint: check if the className includes what you are looking for
+
+	// var playerOne will be an array that will store the class of the tile clicked by player 'X'
+	// var playerTwo will be an array that will store the class of the tile clicked by player 'O'
